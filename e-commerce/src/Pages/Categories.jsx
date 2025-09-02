5d4a7f7cc94d4fb81880892ec1bcbcc1c38a5455
Muhammad Faiq <muhammadfaiqwaseem3@gmail.com>
2025-09-02 08:57:13 +0500

    folder setup in group assignment

---
+++ e-commerce/src/Pages/Categories.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../Components/Navbar'
+import Footer from '../Components/Footer'
+const Categories = () => {
+    return (
+        <div>
+            <Navbar />
+            <h1>Categories</h1>
+
+            <Footer />
+        </div>
+    )
+}
+
+export default Categories
---
+++ e-commerce/src/Pages/Shop.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../Components/Navbar'
+import Footer from '../Components/Footer'
+const Shop = () => {
+    return (
+        <div>
+            <Navbar />
+            <h1>Shop</h1>
+            <Footer />
+        </div>
+    )
+}
+
+export default Shop
