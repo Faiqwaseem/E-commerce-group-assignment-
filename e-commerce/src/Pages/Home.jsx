5a2bbfbac5bc3c319ff662f2792ee41c20ab34f4
Muhammad Faiq <muhammadfaiqwaseem3@gmail.com>
2025-09-12 00:38:41 +0500

    me deletin carsousel and cart

--- e-commerce/src/Pages/Home.jsx
+++ e-commerce/src/Pages/Home.jsx
@@ -1,16 +1,11 @@
 import React from 'react'
 import Navbar from '../Components/Navbar'
-import CarouselsHome from '../Components/CarouselsHome'
 import Footer from '../Components/Footer'
-import Cart from '../Components/Cart'
-
 const Home = () => {
   return (
     <div>
-        <Navbar/>
-        <CarouselsHome />
-        <Cart />
-        <Footer />
+      <Navbar />
+      <Footer />
     </div>
   )
 }
