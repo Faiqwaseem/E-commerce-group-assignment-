1caf84776dca68ca616150f1df105740c4e74ecb
Muhammad Faiq <muhammadfaiqwaseem3@gmail.com>
2025-09-22 18:16:28 +0500

    inconplete log in , sign up page

--- e-commerce/src/Pages/LoginSign.jsx
+++ e-commerce/src/Pages/LoginSign.jsx
@@ -3,11 +3,12 @@ import ShoppingBagIcon from '@mui/icons-material/ShoppingBag';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import GoogleIcon from '@mui/icons-material/Google';
 import TwitterIcon from '@mui/icons-material/Twitter';
+import { useNavigate } from "react-router";
 import { FaShoppingBag, FaEnvelope, FaLock, FaUser, FaFacebookF, FaGoogle, FaTwitter } from 'react-icons/fa';
-import { useQuery } from '@tanstack/react-query';
-import userFetch from '../Services/userFetch';
+
 
 const LoginSign = () => {
+    const navigate = useNavigate()
     useEffect(() => {
         window.scrollTo({
             top: 0,
@@ -16,10 +17,107 @@ const LoginSign = () => {
     }, [])
 
     const [activeForm, setActiveForm] = useState('login');
-    const [fullname, setFullName] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [cPassword, setCpassword] = useState("");
+    const [userEmail, setUserEmail] = useState("");
+    const [userPassword, setUserPassword] = useState("");
+    const [userSignName, setUserSignName] = useState("");
+    const [userSignEmail, setUserSignEmail] = useState("");
+    const [userSignPass, setUserSignPass] = useState("");
+    const [userSignCpass, setUserSignCpass] = useState("");
+
+    const handleLogin = async () => {
+        if (!userEmail || !userPassword) {
+            alert("Invlaid Value")
+            return;
+        }
+        const getData = localStorage.getItem('newUser')
+        if(!getData){
+            alert("No User Save")
+            return;
+        }
+
+        const parsedData = getData ? JSON.parse(getData) : null
+        console.log(parsedData)
+
+        if(parsedData.email === userEmail && parsedData.password === userPassword){
+            alert("Login successfull")
+            navigate('/')
+        }else {
+    alert("Invalid credentials");
+  }
+
+
+        // try {
+        //     const res = await fetch('https://dummyjson.com/auth/login', {
+        //         method: 'POST',
+        //         headers: { 'Content-Type': 'application/json' },
+        //         body: JSON.stringify({
+        //             username: userEmail,
+        //             password: userPassword
+        //         })
+        //     });
+
+        //     const data = await res.json();
+        //     console.log('login response:', data); // yahan token aur user info milega
+        //     if (data.accessToken) {
+        //         localStorage.setItem('token', data.accessToken)
+        //         alert("Log in successfully")
+        //         navigate("/")
+        //     }
+        //     else {
+        //         alert("Invalid credentials");
+        //     }
+        //     // e.g. localStorage.setItem('token', data.token);
+        // } catch (err) {
+        //     console.error('login error:', err);
+
+        // };
+        setUserEmail("")
+        setUserPassword("")
+
+    }
+
+    const handleSign = async () => {
+
+        if (!userSignName || !userSignEmail || !userSignPass || !userSignCpass) {
+            alert("Invlaid Value")
+            return;
+        }
+        if (userSignPass !== userSignCpass) {
+            alert("Passwords do not match");
+            setUserSignCpass("")
+            return;
+        }
+        const userObj = {
+            firstName: userSignName,
+            email: userSignEmail,
+            password: userSignPass
+        }
+
+        try {
+            const res = await fetch('https://dummyjson.com/users/add', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(userObj)
+            });
+            const data = await res.json();
+            console.log('sign up datq===>', data)
+            localStorage.setItem('newUser', JSON.stringify(data))
+            if (data.id) {
+                alert("Account created!");
+                navigate("/"); // ya "/login"
+            }
+        }
+        catch (err) {
+            console.error('login error:', err);
+        }
+
+
+        setUserSignName("");
+        setUserSignEmail("");
+        setUserSignPass("");
+        setUserSignCpass("");
+    }
+
 
     return (
         <div>
@@ -41,12 +139,12 @@ const LoginSign = () => {
                     <form className={`form ${activeForm === 'login' ? 'active' : ''}`} id="login-form">
                         <div className="input-group">
                             <i className="fas fa-envelope"></i>
-                            <input type="email" placeholder="Email Address" required />
+                            <input type="text" value={userEmail} onChange={(e) => setUserEmail(e.target.value)} placeholder="Email Address" required />
                         </div>
 
                         <div className="input-group">
                             <i className="fas fa-lock"></i>
-                            <input type="password" placeholder="Password" required />
+                            <input type="password" value={userPassword} onChange={(e) => setUserPassword(e.target.value)} placeholder="Password" required />
                         </div>
 
                         <div className="remember-forgot">
@@ -56,7 +154,7 @@ const LoginSign = () => {
                             <a href="#" className="forgot">Forgot Password?</a>
                         </div>
 
-                        <button type="submit" className="submit-btn">Login</button>
+                        <button type="button" className="submit-btn" onClick={handleLogin}>Login</button>
 
                         <div className="social-login">
                             <p>Or login with</p>
@@ -72,49 +170,30 @@ const LoginSign = () => {
                                 </div>
                             </div>
                         </div>
-                    </form> 
+                    </form>
 
                     <form className={`form ${activeForm === 'signup' ? 'active' : ""}`} id="signup-form">
                         <div className="input-group">
                             <i className="fas fa-user"></i>
-                            <input
-                                value={fullname}
-                                onChange={(e) =>
-                                    setFullName(e.target.value)
-                                } type="text" placeholder="Full Name" required />
+                            <input type="text" value={userSignName} onChange={(e) => setUserSignName(e.target.value)} placeholder="Full Name" required />
                         </div>
 
                         <div className="input-group">
                             <i className="fas fa-envelope"></i>
-                            <input
-                                value={email}
-                                onChange={(e) =>
-                                    setEmail(e.target.value)
-                                } type="email" placeholder="Email Address" required />
+                            <input type="email" value={userSignEmail} onChange={(e) => setUserSignEmail(e.target.value)} placeholder="Email Address" required />
                         </div>
 
                         <div className="input-group">
                             <i className="fas fa-lock"></i>
-                            <input
-                                value={password}
-                                
-                                onChange={(e) =>
-                                    setPassword(e.target.value)
-                                }
-                                type="password" placeholder="Password" required />
+                            <input type="password" value={userSignPass} onChange={(e) => setUserSignPass(e.target.value)} placeholder="Password" required />
                         </div>
 
                         <div className="input-group">
                             <i className="fas fa-lock"></i>
-                            <input
-                                value={cPassword}
-                                onChange={(e) =>
-                                    setCpassword(e.target.value)
-                                }
-                                type="password" placeholder="Confirm Password" required />
+                            <input type="password" value={userSignCpass} onChange={(e) => setUserSignCpass(e.target.value)} placeholder="Confirm Password" required />
                         </div>
 
-                        <button type="submit" className="submit-btn">Create Account</button>
+                        <button type="button" className="submit-btn" onClick={handleSign}>Create Account</button>
 
                         <div className="social-login">
                             <p>Or sign up with</p>
--- e-commerce/src/Services/userFetch.js
+++
@@ -1,5 +0,0 @@
-const userFetch = async () => {
-    const res = await fetch('https://dummyjson.com/users');
-    return res.json()
-}
- export default userFetch;
